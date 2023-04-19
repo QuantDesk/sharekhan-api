1a8dd2ccdd5c0d7ea29cf841bc9da05d32655c37
Harsh Navadiya <hnavadiya1802@gmail.com>
2023-04-19 13:50:00 +0530

    update

--- report_downloader.py
+++ report_downloader.py
@@ -3,23 +3,13 @@ import pandas as pd
 import schedule
 import time
 
-SAVE_PATH = ''
 
+SAVE_PATH = r'./mbs100_sk.txt'
 
 deta = Deta('d0rwyaghixk_xdYc2J98omec5FU2bEyrePtiYZXPwHPF')
 sharekhan_keys_db = deta.Base('sharekhan-keys')
 report_drive = deta.Drive('report')
-
-report_file = report_drive.get('mbs100_sk.txt')
-file_data = report_file.read().decode()
-
-
-with open('D:\VisIOT\TESTTT\mbs100_sk.txt','w') as f:
-    for line in file_data:
-        f.write(line)
-
-    f.close()
-
+print("Deta setup done!")
 
 def download_report():
     report_file = report_drive.get('mbs100_sk.txt')
@@ -32,12 +22,15 @@ def download_report():
 
         f.close()
 
+    print(f'New report updated at {time.ctime()}')
+
 
 schedule.every(2).minutes.do(download_report)
-
+print(schedule.get_jobs())
 
 while 1:
     schedule.run_pending()
     time.sleep(5)
+
 
 
--- report_generator.py
+++ report_generator.py
@@ -1,6 +1,7 @@
 from deta import Deta
 from sharekhanConnect import SharekhanConnect
 import pandas as pd
+import time
 
 
 deta = Deta('d0rwyaghixk_xdYc2J98omec5FU2bEyrePtiYZXPwHPF')
@@ -17,10 +18,6 @@ version_id = "1005"
 
 sharekhan = SharekhanConnect(api_key,access_token=access_token)
 
-report = sharekhan.reports(customer_id)
-
-report_df = pd.DataFrame(report['data'])
-
 def date_converter(date):
     date_split = date.split()
     date = date_split[1]
@@ -32,39 +29,49 @@ def date_converter(date):
 
     return converted
 
-df = pd.DataFrame({})
+while 1:
+
+    report = sharekhan.reports(customer_id)
+
+    report_df = pd.DataFrame(report['data'])
 
 
-df['Trade No'] = report_df['exchOrderId']
-df['Trade Status'] = 11
-df['Instrument Name'] = 'OPTIDX'
-df['Symbol'] = report_df['contract']
-df['Expiry Date'] = report_df['tradingSymbol'].apply(date_converter)
-df['Strike Price'] = report_df['strikePrice']
-df['Option Type'] = report_df['optionType']
-df['Security Name'] = report_df['tradingSymbol']
-df['Book Type'] = 1
-df['Book Type Name'] = 'RL'
-df['Market Type'] = 1
-df['User ID'] = "MBS100"
-df['Branch ID'] = 14
-df['BuySell'] = report_df.apply(lambda x : 1 if x['buySell'] == "B" else 2,axis=1)
-df['Qty Traded'] = report_df['orderQty']
-df['Price'] = report_df['execPrice']
-df['Pro/Cli'] = 1
-df['Client AC'] = 2683052
-df['Participant Code'] = 10733
-df['Open Close'] = "OPEN"
-df['Cover UnCover'] = ""
-df['Entry Time'] = report_df['lastModTime']
-df['Modify DateTime'] = report_df['lastModTime']
-df['Order No'] = report_df['exchOrderId']
-df['CP Id'] = 'NIL'
-df['ProductType'] = 'CarryForward'
 
-df.to_csv("mbs100_sk.txt",header=None)
+    df = pd.DataFrame({})
 
-f = open('./mbs100_sk.txt', 'r')
-report_drive.put('mbs100_sk.txt', f)
-f.close()
+
+    df['Trade No'] = report_df['exchOrderId']
+    df['Trade Status'] = 11
+    df['Instrument Name'] = 'OPTIDX'
+    df['Symbol'] = report_df['contract']
+    df['Expiry Date'] = report_df['tradingSymbol'].apply(date_converter)
+    df['Strike Price'] = report_df['strikePrice']
+    df['Option Type'] = report_df['optionType']
+    df['Security Name'] = report_df['tradingSymbol']
+    df['Book Type'] = 1
+    df['Book Type Name'] = 'RL'
+    df['Market Type'] = 1
+    df['User ID'] = "MBS100"
+    df['Branch ID'] = 14
+    df['BuySell'] = report_df.apply(lambda x : 1 if x['buySell'] == "B" else 2,axis=1)
+    df['Qty Traded'] = report_df['orderQty']
+    df['Price'] = report_df['execPrice']
+    df['Pro/Cli'] = 1
+    df['Client AC'] = 2683052
+    df['Participant Code'] = 10733
+    df['Open Close'] = "OPEN"
+    df['Cover UnCover'] = ""
+    df['Entry Time'] = report_df['lastModTime']
+    df['Modify DateTime'] = report_df['lastModTime']
+    df['Order No'] = report_df['exchOrderId']
+    df['CP Id'] = 'NIL'
+    df['ProductType'] = 'CarryForward'
+
+    df.to_csv("mbs100_sk.txt",header=None)
+
+    f = open('./mbs100_sk.txt', 'r')
+    report_drive.put('mbs100_sk.txt', f)
+    f.close()
+
+    time.sleep(180)
 
